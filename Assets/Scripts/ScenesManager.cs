02a125e5eaa3aa855bfb4813e8793c3f53f8b5eb
Kiki <108219879+noemykiki@users.noreply.github.com>
2024-05-03 18:48:47 +0200

    Added a pause menu
    - Added a functioning pause menu to main scene

--- Assets/Scripts/ScenesManager.cs
+++ Assets/Scripts/ScenesManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ScenesManager : MonoBehaviour
 {
+
+    public GameObject pauseMenu; 
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,20 @@ public class ScenesManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf == false)
+            {
+                // Toggle the active state of the target object
+                pauseMenu.SetActive(true);
+            }
+            else if (pauseMenu.activeSelf == true)
+            {
+                // Toggle the active state of the target object
+                pauseMenu.SetActive(false);
+            }
+           
+        }
     }
 
     public void Play()
@@ -26,6 +42,11 @@ public class ScenesManager : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("QUIT");
+    }
+
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene("Title Screen");
     }
     
     
